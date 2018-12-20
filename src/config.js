3e35b7cd69d3b54f41c916488e1836d1e007bc3a
Serigne <seloweb810@gmail.com>
2018-12-20 01:27:38 +0000

    first commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yzio2yGPW",
+    USER_POOL_ID: "YOUR_USER_POOL_ID",
     APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
     IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
   }
